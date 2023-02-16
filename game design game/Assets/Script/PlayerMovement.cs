1e442a7dc4e55b5a7f2627c6da268bc83680a164
Jocelynleong25 <leong.jocelyn@maranathastudents.org>
2023-02-15 21:40:28 -0800

    2/15/23 the attack animations aren't working

--- game design game/Assets/Script/PlayerMovement.cs
+++ game design game/Assets/Script/PlayerMovement.cs
@@ -2,15 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerState
+{
+    walk,
+    attack,
+    interact
+
+} 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     private Rigidbody2D myRigidbody;
     private Vector3 change;
-    private Animator animator; 
+    private Animator animator;
+    public PlayerState currentState; 
     // Start is called before the first frame update
     void Start()
     {
+        currentState = PlayerState.walk;
         animator = GetComponent<Animator>();
         myRigidbody = GetComponent<Rigidbody2D>();
     }
@@ -21,8 +30,27 @@ public class PlayerMovement : MonoBehaviour
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
-        UpdateAnimationAndMove();
+        if(Input.GetButtonDown("attack") && currentState != PlayerState.attack)
+        {
+            StartCoroutine(AttackCo());
+        }
+        else if(currentState == PlayerState.walk)
+        {
+            UpdateAnimationAndMove();
+        }
+           
         
+    }
+
+    private IEnumerable AttackCo()
+    {
+        animator.SetBool("attacking", true);
+        currentState = PlayerState.attack;
+        yield return null;
+        animator.SetBool("attacking", false);
+        yield return new WaitForSeconds(.3f);
+        currentState = PlayerState.walk;
+
     }
 
     void UpdateAnimationAndMove()
