fd74ad49cd492f832c44c1d7dafefdbd648f6bc0
Jocelynleong25 <leong.jocelyn@maranathastudents.org>
2023-03-01 11:40:10 -0800

    march 1

---
+++ game design game/Assets/Script/log.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class log : Enemy   
+{
+    public Transform target;
+    public float chaseRadius;
+    public float attackRadius;
+    public Transform homePosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckDistance();
+    }
+
+    void CheckDistance()
+    {
+        if(Vector3.Distance(target.position, transform.position) <= chaseRadius)
+        {
+            transform.position = Vector3.MoveTowards(target.position, target.position, moveSpeed * Time.deltaTime);
+        }
+    }
+
+
+}
