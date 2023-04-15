5ca6e2de0d4030c8d73ad6c855ffe367a8ef64d5
Jocelynleong25 <leong.jocelyn@maranathastudents.org>
2023-04-15 14:53:32 -0700

    4/15/23 knockback works

--- game design game/Assets/Script/Knockback.cs
+++ game design game/Assets/Script/Knockback.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Knockback : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float thrust;
+    public float knockTime;
+   
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if(other.gameObject.CompareTag("enemy"))
+        {
+            Rigidbody2D enemy = other.GetComponent<Rigidbody2D>();
+            if (enemy != null)
+            {
+                enemy.isKinematic = false;
+                Vector2 difference = enemy.transform.position - transform.position;
+                difference = difference.normalized * thrust;
+                enemy.AddForce(difference, ForceMode2D.Impulse);
+                StartCoroutine(KnockCo(enemy));
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator KnockCo(Rigidbody2D enemy)
     {
-        
+        if(enemy != null)
+        {
+           yield return new WaitForSeconds(knockTime);
+           enemy.velocity = Vector2.zero;
+           enemy.isKinematic = true; 
+        }
     }
 }
